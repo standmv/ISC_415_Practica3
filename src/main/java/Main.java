eee2bd62ef54a41a5656a31e6da249d17ace9f93
Stanley De Moya <standmv13@gmail.com>
2018-06-11 00:37:00 -0400

    Hacer que se vea un home page

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+import static spark.Spark.*;
+import freemarker.template.*;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        staticFiles.location("/templates/html");
+
+        final Configuration configuration = new Configuration(new Version(2,3,0));
+        configuration.setClassForTemplateLoading(Main.class, "/");
+
+
+        get("/", (request, response) -> {
+   /*         Template homeTemplate = configuration.getTemplate("home.ftl");
+            StringWriter stringWriter = new StringWriter();
+
+            Map<String, Object> params = new HashMap<>();
+            params.put("title", "Practica 2 de Programacion Web ISC-415 CRUD");
+            params.put("header", "Estudiantes:");
+            params.put("insertEstudiante", "Insertar un estudiante nuevo");
+
+            homeTemplate.process(params, stringWriter);*/
+            return configuration.getTemplate("templates/html/home.ftl");
+        });
+
+    }
+}
